2ef87cb7351e425c90d7cd83770158d7a736ba66
Dick Larsson <dicklarsson@gmail.com>
2023-01-21 18:33:58 +0100

    Handle node.js process errors like a gentleman

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ const { whitelist, isWhitelisted } = require("./whitelist");
 2) Most requests are cached for the lifespan of the current block
 
 */
+process.on('uncaughtException', (error, origin) => {
+    console.log('----- Uncaught exception -----')
+    console.log(error)
+    console.log('----- Exception origin -----')
+    console.log(origin)
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('----- Unhandled Rejection at -----')
+    console.log(promise)
+    console.log('----- Reason -----')
+    console.log(reason)
+});
 
 /*
 The cache mechanism uses getbestblockhash to determine when to invalidate the cache
