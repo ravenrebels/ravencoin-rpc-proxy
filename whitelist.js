d0ddba3a6dc1ce988f6d4409cd14f7f58c3155d5
Dick H Larsson <dicklarsson@gmail.com>
2024-01-02 19:25:36 +0100

    Unblock getnetworkhashps procedure

--- whitelist.js
+++ whitelist.js
@@ -1,140 +1,140 @@
 const whitelist = [
-    //== Addressindex ==
-    "getaddressbalance",
-    "getaddressdeltas",
-    "getaddressmempool",
-    "getaddresstxids",
-    "getaddressutxos",
+  //== Addressindex ==
+  "getaddressbalance",
+  "getaddressdeltas",
+  "getaddressmempool",
+  "getaddresstxids",
+  "getaddressutxos",
 
-    //== Assets ==
-    "getassetdata",
-    // "getcacheinfo",
-    //"getsnapshot",
-    //"issue",
-    //"issueunique",
-    "listaddressesbyasset",
-    "listassetbalancesbyaddress",
-    "listassets",
-    //"listmyassets",
-    //"purgesnapshot",
-    //"reissue",
-    //"transfer",
-    //"transferfromaddress",
-    //"transferfromaddresses",
+  //== Assets ==
+  "getassetdata",
+  // "getcacheinfo",
+  //"getsnapshot",
+  //"issue",
+  //"issueunique",
+  "listaddressesbyasset",
+  "listassetbalancesbyaddress",
+  "listassets",
+  //"listmyassets",
+  //"purgesnapshot",
+  //"reissue",
+  //"transfer",
+  //"transferfromaddress",
+  //"transferfromaddresses",
 
-    //== Blockchain ==
-    //"clearmempool",
-    "decodeblock",
-    "getbestblockhash",
-    "getblock",
-    "getblockchaininfo",
-    "getblockcount",
-    "getblockhash",
-   // "getblockhashes", //This can kill the service if you ask for all block hashes years back
-    "getblockheader",
-    "getchaintips",
-    "getchaintxstats",
-    "getdifficulty",
-    "getmempoolancestors",
-    "getmempooldescendants",
-    "getmempoolentry",
-    "getmempoolinfo",
-    "getrawmempool",
-    "getspentinfo",
-    "gettxout",
-    "gettxoutproof",
-   // "gettxoutsetinfo", this method is not "dangerous" but it takes TOO long time
-    //"preciousblock",
-    //"pruneblockchain",
-    //"savemempool",
-    //"verifychain",
-    //"verifytxoutproof",
+  //== Blockchain ==
+  //"clearmempool",
+  "decodeblock",
+  "getbestblockhash",
+  "getblock",
+  "getblockchaininfo",
+  "getblockcount",
+  "getblockhash",
+  // "getblockhashes", //This can kill the service if you ask for all block hashes years back
+  "getblockheader",
+  "getchaintips",
+  "getchaintxstats",
+  "getdifficulty",
+  "getmempoolancestors",
+  "getmempooldescendants",
+  "getmempoolentry",
+  "getmempoolinfo",
+  "getrawmempool",
+  "getspentinfo",
+  "gettxout",
+  "gettxoutproof",
+  // "gettxoutsetinfo", this method is not "dangerous" but it takes TOO long time
+  //"preciousblock",
+  //"pruneblockchain",
+  //"savemempool",
+  //"verifychain",
+  //"verifytxoutproof",
 
-    //== Control ==
-    //"getinfo",
-    //"getmemoryinfo",
-    //"getrpcinfo",
-    "help",
-    //"stop",
-    //"uptime",
+  //== Control ==
+  //"getinfo",
+  //"getmemoryinfo",
+  //"getrpcinfo",
+  "help",
+  //"stop",
+  //"uptime",
 
-    //== Generating ==
-    //"generate",
-    //"generatetoaddress",
-    //"getgenerate",
-    //"setgenerate",
+  //== Generating ==
+  //"generate",
+  //"generatetoaddress",
+  //"getgenerate",
+  //"setgenerate",
 
-    //== Messages ==
-    //"clearmessages",
-    //"sendmessage",
-    //"subscribetochannel",
-    //"unsubscribefromchannel",
-    //"viewallmessagechannels",
-    //"viewallmessages",
+  //== Messages ==
+  //"clearmessages",
+  //"sendmessage",
+  //"subscribetochannel",
+  //"unsubscribefromchannel",
+  //"viewallmessagechannels",
+  //"viewallmessages",
 
-    //== Mining ==
-   // "getblocktemplate",
-    //"getkawpowhash",
-    //"getmininginfo",
-    //"getnetworkhashps",
-    //"pprpcsb",
-    //"prioritisetransaction",
-    //"submitblock",
+  //== Mining ==
+  // "getblocktemplate",
+  //"getkawpowhash",
+  //"getmininginfo",
+  "getnetworkhashps",
+  //"pprpcsb",
+  //"prioritisetransaction",
+  //"submitblock",
 
-    //== Network ==
-    //"addnode",
-    //"clearbanned",
-    //"disconnectnode",
-    //"getaddednodeinfo",
-    //"getconnectioncount",
-    //"getnettotals",
-    //"getnetworkinfo",
-    //"getpeerinfo",
-    //"listbanned",
-    //"ping",
-    //"setban",
-    //"setnetworkactive",
+  //== Network ==
+  //"addnode",
+  //"clearbanned",
+  //"disconnectnode",
+  //"getaddednodeinfo",
+  //"getconnectioncount",
+  //"getnettotals",
+  //"getnetworkinfo",
+  //"getpeerinfo",
+  //"listbanned",
+  //"ping",
+  //"setban",
+  //"setnetworkactive",
 
-    //== Rawtransactions ==
-    "combinerawtransaction",
-    "createrawtransaction",
-    "decoderawtransaction",
-    "decodescript",
-    //"fundrawtransaction",
-    "getrawtransaction",
-    "sendrawtransaction",
-    "signrawtransaction",
-    "testmempoolaccept",
+  //== Rawtransactions ==
+  "combinerawtransaction",
+  "createrawtransaction",
+  "decoderawtransaction",
+  "decodescript",
+  //"fundrawtransaction",
+  "getrawtransaction",
+  "sendrawtransaction",
+  "signrawtransaction",
+  "testmempoolaccept",
 
-    //== Restricted assets ==
-    // "addtagtoaddress",
-    "checkaddressrestriction",
-    "checkaddresstag",
-    "checkglobalrestriction",
-    //"freezeaddress",
-    //"freezerestrictedasset",
-    "getverifierstring",
-    //"issuequalifierasset",
-    //"issuerestrictedasset",
-    "isvalidverifierstring",
-    "listaddressesfortag",
-    "listaddressrestrictions",
-    "listglobalrestrictions",
-    "listtagsforaddress",
-    //"reissuerestrictedasset",
-    //"removetagfromaddress",
-    //"transferqualifier",
-    //"unfreezeaddress",
-    //"unfreezerestrictedasset",
+  //== Restricted assets ==
+  // "addtagtoaddress",
+  "checkaddressrestriction",
+  "checkaddresstag",
+  "checkglobalrestriction",
+  //"freezeaddress",
+  //"freezerestrictedasset",
+  "getverifierstring",
+  //"issuequalifierasset",
+  //"issuerestrictedasset",
+  "isvalidverifierstring",
+  "listaddressesfortag",
+  "listaddressrestrictions",
+  "listglobalrestrictions",
+  "listtagsforaddress",
+  //"reissuerestrictedasset",
+  //"removetagfromaddress",
+  //"transferqualifier",
+  //"unfreezeaddress",
+  //"unfreezerestrictedasset",
 
-    //== Restricted ==
-    /*
+  //== Restricted ==
+  /*
     "viewmyrestrictedaddresses",
     "viewmytaggedaddresses",
     */
 
-    //== Rewards ==
-    /*
+  //== Rewards ==
+  /*
     "cancelsnapshotrequest",
     "distributereward",
     "getdistributestatus",
@@ -144,16 +144,16 @@ const whitelist = [
 
     */
 
-    //== Util ==
-    //"createmultisig",
-    "estimatefee",
-    "estimatesmartfee",
-    "signmessagewithprivkey",
-    "validateaddress",
-    "verifymessage",
+  //== Util ==
+  //"createmultisig",
+  "estimatefee",
+  "estimatesmartfee",
+  "signmessagewithprivkey",
+  "validateaddress",
+  "verifymessage",
 
-    //== Wallet ==
-    /*
+  //== Wallet ==
+  /*
     "abandontransaction",
     "abortrescan",
     "addmultisigaddress",
@@ -204,14 +204,13 @@ const whitelist = [
     "settxfee",
     "signmessage",
      */
-]
+];
 
- 
 function isWhitelisted(method) {
-    const inc = whitelist.includes(method) 
-    return inc;
-
+  const inc = whitelist.includes(method);
+  return inc;
 }
 module.exports = {
-    whitelist, isWhitelisted
+  whitelist,
+  isWhitelisted,
 };
